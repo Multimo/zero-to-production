267cb7c6a291a2853adcc5fb085c419c6b9cdb0d
Tim Mulqueen <tmulqueen@box.com>
2023-05-02 09:43:07 +0200

    fix: experiment making own test spwaner

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,13 +1,23 @@
+use std::net::{SocketAddr, TcpListener};
+
+use axum::Server;
 use axum_test_helper::TestClient;
 
 #[tokio::test]
-async fn health_check_words() {
-    let client = spawn_app();
+async fn health_check_works() {
+    let port = async_spawn_app().await;
 
-    let response = client.get("/health_check").send().await;
+    println!("Found port {:?}", port);
+
+    let address = format!("http://{}", port);
+    let url: String = format!("{}/health_check", address);
+
+    println!("Found url {:?}", url);
+
+    let response = reqwest::get(url).await.expect("Could not get response");
 
     assert!(response.status().is_success());
-    let test_response = response.text().await;
+    let test_response = response.text().await.expect("Could not get text content");
     assert_eq!("", test_response);
 }
 
@@ -15,4 +25,21 @@ fn spawn_app() -> TestClient {
     let app = zero_to_production::run();
 
     TestClient::new(app)
+}
+
+async fn async_spawn_app() -> SocketAddr {
+    let app = zero_to_production::run();
+
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Could not bind ephemeral socket");
+    let addr: std::net::SocketAddr = listener.local_addr().unwrap();
+    println!("Listening on {}", addr);
+
+    tokio::spawn(async move {
+        let server = Server::from_tcp(listener)
+            .unwrap()
+            .serve(app.into_make_service());
+        server.await.expect("server error");
+    });
+
+    addr
 }
